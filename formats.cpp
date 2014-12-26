53a57c4b495e774bf61418eabd1ae9d55c7a1917
Yuri Feldman <yuri@tbqh.net>
2014-12-27 00:58:51 +1100

    cleanup

--- formats.cpp
+++ formats.cpp
@@ -93,6 +93,7 @@ int main(int argc, char *argv[])
   uint8 One = 0xff;
   PrintBits(&One, 1);
 
+  // TODO(yuri): Implement padding
   uint8 Base64Padding = (ByteBufferSize % 3 != 0) ? (3 - (ByteBufferSize % 3)) : 0;
   uint8 PaddedByteBufferSize = ByteBufferSize + Base64Padding;
   uint8 Base64Length = PaddedByteBufferSize * 8 / 6;
@@ -110,11 +111,11 @@ int main(int argc, char *argv[])
   uint8 Mask = 0xff;
 
   uint8 Base64LookupTable[] = {
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
-    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
-    '8', '9', '+', '/'
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
   };
 
   for(int TripletIndex = 0;
