a80403733e4524c3bd8322ce77eed1af01be7ae7
Yuri Feldman <yuri@tbqh.net>
2015-01-24 21:10:41 +1100

    Copypasta

--- cryptopals.cpp
+++ cryptopals.cpp
@@ -1335,7 +1335,7 @@ AES256Test(void)
     AESTest("00112233445566778899aabbccddeeff",
             "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
             "8ea2b7ca516745bfeafc49904b496089");
-    printf("AES-192 test passed!\n");
+    printf("AES-256 test passed!\n");
 }
 
 
